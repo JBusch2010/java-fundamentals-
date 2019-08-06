252a9724471d0b4d883e5d54357bfc1f9c247224
Joachen D Busch <joachendbusch@gmail.com>
2019-08-06 12:54:08 -0700

    feature 1 done

--- basics/Main.java
+++ basics/Main.java
@@ -1,5 +1,21 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
+    int dogCount = 1;
+System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+int catCount = 2;
+System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+int turtleCount = 0;
+System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+  }
+
+  public static String pluralize(String pet, int count) {
+    if (count == 0 || count > 1) {
+      return pet + "s";
+    } else {
+      return pet;
+    }
   }
 }
