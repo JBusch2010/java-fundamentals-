d9df942133d54199aaf4425f1ac60867f826ed0d
Joachen D Busch <joachendbusch@gmail.com>
2019-08-06 20:49:22 -0700

    completed 1-2

--- basics/Main.java
+++ basics/Main.java
@@ -9,6 +9,8 @@ System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".")
 
 int turtleCount = 0;
 System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+
+System.out.println(flipNHeads(2));
   }
 
   public static String pluralize(String pet, int count) {
@@ -17,5 +19,25 @@ System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCoun
     } else {
       return pet;
     }
+  }
+
+  public static String flipNHeads(int n) {
+    int timesFlipped = 0;
+    int headsCounter = 0;
+
+    while (headsCounter < n) {
+      double rand = Math.random();
+      timesFlipped++;
+
+      if (rand >= 0.5) {
+        headsCounter++;
+        System.out.println("heads!");
+      } else {
+        System.out.println("tails!");
+        headsCounter = 0;
+      }
+    }
+
+    return "It took " + timesFlipped + " flips to flip " + headsCounter + " heads in a row.";
   }
 }
